73e8218bc8bcd97ff1f9c510d9489f1d7b261e86
alexpotrich <73121810+alexpotrich@users.noreply.github.com>
2021-05-19 16:40:26 +0200

    Delete forum directory

--- forum/img.php
+++
@@ -1,8 +0,0 @@
-<?php
-include 'connectbdd.php';
-$id = $_GET['id'];
-$req = $bdd->prepare('SELECT pdp FROM (SELECT idAdmin AS id_user, nomAdmin AS nom_user, prenomAdmin AS prenom_user, photoAdmin AS pdp FROM administrateur UNION SELECT idCoach, nomCoach, prenomCoach, photoCoach FROM coach UNION SELECT idJoueur, nomJoueur, prenomJoueur, photoJoueur FROM joueur) AS u WHERE id_user = ?');
-$req->execute(array($id));
-$donnees = $req->fetch();
-echo $donnees['pdp'];
-?>
